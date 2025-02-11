a9f680220c03079d141ba476e004ea61ce19d06a
Bonjoureloi <151631854+Bonjoureloi@users.noreply.github.com>
2025-02-11 16:51:06 +0100

    LigneCommande

--- src/main/java/model/LigneCommande.java
+++ src/main/java/model/LigneCommande.java
@@ -1,5 +1,54 @@
 package model;
 
-public class LigneCommande {
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "LigneCommande")
+public class LigneCommande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idLigneCommande")
+    private Integer idLigneCommande;
+
+    @Column(name = "quantite")
+    private Integer quantite;
+
+    @ManyToOne
+    @JoinColumn(name = "idCommande")
+    private Commande commande;
+
+    public LigneCommande() {
+    }
+
+    public LigneCommande(Integer idLigneCommande, Integer quantite, Commande commande) {
+        this.idLigneCommande = idLigneCommande;
+        this.quantite = quantite;
+        this.commande = commande;
+    }
+
+    public Integer getIdLigneCommande() {
+        return idLigneCommande;
+    }
+
+    public void setIdLigneCommande(Integer idLigneCommande) {
+        this.idLigneCommande = idLigneCommande;
+    }
+
+    public Integer getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(Integer quantite) {
+        this.quantite = quantite;
+    }
+
+    public Commande getCommande() {
+        return commande;
+    }
+
+    public void setCommande(Commande commande) {
+        this.commande = commande;
+    }
 }
