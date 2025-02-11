b0a78d810ce8f909720f849a754620616306b0e1
Bonjoureloi <151631854+Bonjoureloi@users.noreply.github.com>
2025-02-11 15:27:29 +0100

    解决冲突并提交修改

---
+++ src/main/java/model/statut.java
@@ -0,0 +1,13 @@
+package model;
+
+public enum statut {
+    PROCESSING,
+    READY_FOR_SHIPMENT,
+    SHIPPED,
+    IN_TRANSIT,
+    DELIVERED,
+    COMPLETED,
+    CANCELLED,
+    RETURNED,
+    REFUNDED;
+}
